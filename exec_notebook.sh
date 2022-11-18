a200e0121489560c9575851c6acbc30f6f8d5a76
masaru gotou <masaru.gotou@gmail.com>
2022-11-19 00:00:53 +0900

    change

--- exec_notebook.sh
+++ exec_notebook.sh
@@ -5,11 +5,11 @@
 log "start `date`"
 #DEBUG=--debug
 #jupyter nbconvert --to notebook --inplace --execute covid-19-jp-nhk.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-nhk-jp.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-jp-nhk-pref-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-#jupyter nbconvert --to notebook --inplace --execute covid-19-jp-nhk-pref-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-#jupyter nbconvert --to notebook --inplace --execute covid-19-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-#jupyter nbconvert --to notebook --inplace --execute covid-19-global-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+#jupyter nbconvert --to notebook --inplace --execute covid-19-nhk-jp.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+# jupyter nbconvert --to notebook --inplace --execute covid-19-jp-nhk-pref-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --inplace --execute covid-19-jp-gov-all.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --inplace --execute covid-19-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --inplace --execute covid-19-global-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --inplace --execute covid-19-owid-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --inplace --execute covid-19-owid-global-vaccine.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 
