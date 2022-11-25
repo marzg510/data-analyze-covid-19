174436bbb6ccaf55770dfd1ca2355785c0b9bfda
masaru gotou <masaru.gotou@gmail.com>
2022-11-25 16:57:47 +0900

    add gov pref

--- exec_notebook.sh
+++ exec_notebook.sh
@@ -8,10 +8,10 @@ log "start `date`"
 #jupyter nbconvert --to notebook --inplace --execute covid-19-nhk-jp.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 # jupyter nbconvert --to notebook --inplace --execute covid-19-jp-nhk-pref-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --inplace --execute covid-19-jp-gov-all.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --inplace --execute covid-19-jp-gov-pref.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --inplace --execute covid-19-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --inplace --execute covid-19-global-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --inplace --execute covid-19-owid-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --inplace --execute covid-19-owid-global-vaccine.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-
 
 log "end `date`"
