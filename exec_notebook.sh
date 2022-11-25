aa139d2f07fe3f1a1d6b3e17ff391c18f2f840f7
masaru gotou <masaru.gotou@gmail.com>
2022-11-25 22:12:10 +0900

    ipynbを書き換えないように修正

--- exec_notebook.sh
+++ exec_notebook.sh
@@ -7,11 +7,12 @@ log "start `date`"
 #jupyter nbconvert --to notebook --inplace --execute covid-19-jp-nhk.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 #jupyter nbconvert --to notebook --inplace --execute covid-19-nhk-jp.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 # jupyter nbconvert --to notebook --inplace --execute covid-19-jp-nhk-pref-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-jp-gov-all.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-jp-gov-pref.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-global-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-owid-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --inplace --execute covid-19-owid-global-vaccine.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+# jupyter nbconvert --to notebook --inplace --execute covid-19-jp-gov-all.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --execute covid-19-jp-gov-all.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --execute covid-19-jp-gov-pref.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --execute covid-19-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --execute covid-19-global-latest.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --execute covid-19-owid-global-daily.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+jupyter nbconvert --to notebook --execute covid-19-owid-global-vaccine.ipynb --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 
 log "end `date`"
