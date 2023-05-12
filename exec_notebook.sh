ca29c0a3f412efc622eefbb7e1997385e3f74e93
Masaru Gotou <masaru.gotou@gmail.com>
2023-05-13 00:44:36 +0900

    日本のデータの集計を停止

--- exec_notebook.sh
+++ exec_notebook.sh
@@ -4,8 +4,8 @@
 
 log "start `date`"
 #DEBUG=--debug
-jupyter nbconvert --to notebook --execute covid-19-jp-gov-all.ipynb --output-dir=out/ --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
-jupyter nbconvert --to notebook --execute covid-19-jp-gov-pref.ipynb --output-dir=out/ --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+#jupyter nbconvert --to notebook --execute covid-19-jp-gov-all.ipynb --output-dir=out/ --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
+#jupyter nbconvert --to notebook --execute covid-19-jp-gov-pref.ipynb --output-dir=out/ --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --execute covid-19-global-daily.ipynb --output-dir=out/ --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --execute covid-19-global-latest.ipynb --output-dir=out/ --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
 jupyter nbconvert --to notebook --execute covid-19-owid-global-daily.ipynb --output-dir=out/ --ExecutePreprocessor.timeout=1800 --allow-errors $DEBUG |& log
